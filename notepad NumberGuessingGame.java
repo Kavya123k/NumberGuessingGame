cf4285695ee0c9f396429135fccbc815e9c89e44
Kavya123k <kavyabs.129@gmail.com>
2025-12-14 15:24:16 +0530

    guessing

---
+++ notepad NumberGuessingGame.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int secretNumber = random.nextInt(100) + 1;
+        int guess;
+        int attempts = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Guess a number between 1 and 100");
+
+        while (true) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < secretNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the correct number.");
+                System.out.println("Total attempts: " + attempts);
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+}
